4a78782eda861f41dcd4a9a6057a446a927ed385
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2020-10-09 10:39:55 -1000

    done with post

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,34 +1,54 @@
 const express = require('express');
 const mongoose = require('mongoose');
 // const {MongoClient} = require('mongodb');
-const { v4:uuidv4 } = require('uuid');
-const respSchema=require('../Model/responseSchema');
 
-// let resp={};
+const respSchema=require('../Model/responseSchema');
 
 const router=express.Router();
 router.use(express.json());
 // console.log("Routes here");
 
-router.put("/meet/:meetId", (req, res) => {
-    console.log("Hey!!");
-    res.send(req.body);
-    
-})
-router.post("/meet", async (req,res)=>{
+router.put("/response/:responseId", async (req, res) => {
+        const responseId = req.params.responseId; 
+        const response = respSchema.findOne({"responseId":responseId});
+        req.json({"response":response});
 
+})
+//curl testing
+/*
+curl --header "Content-Type: application/json" --request POST --data '
+{
+"form_id":"32",
+"responseBy":"kumarnitesh2000.nk@gmail.com",
+"section_list":[
+  [{
+  "is_choice":false,
+  "name":"Nitesh Kumar Arora"
+},{
+  "is_choice":true,
+  "gender":{"male":1,"female":0,"others":0}
+},
+{
+  "is_choice":false,
+  "roll-number":1806810217
+}
+]
+]
+
+}' http://localhost:6000/api/meet
+*/
+
+router.post("/response", async (req,res)=>{
+    console.log(req.body);
+    
     let resp=new respSchema({
         formId:req.body.form_id,
-        responseId:uuidv4(),
         responseBy:req.body.responseBy,
         sections:req.body.section_list
     });
-    console.log(resp);
-    // res.send(resp);
 
     //saving to mongoDB
     const idSave = await resp.save();
-    console.log("SavedId",idSave);
     res.send(idSave);
 });
 // const app = express();
--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const formRoute = require('./Routes/routes');
 
 const app = express();
 app.use(cors());
-console.log('FormResponse service Activated..!!');
+
 
 const port = process.env.port || 6000;
-const uri = process.env.URI || "https://mongodb+srv://hardikshah:8630877372@hs@mycluster.hb4pm.mongodb.net/Test?retryWrites=true&w=majority" ;
-
+const uri = process.env.URI ;
+console.log('FormResponse service Activated..!! on port '+port);
 //tester middleware
 app.use(express.json());
+
+
 const testFun = require('./MiddleWareFun/auth');
 app.use(testFun);
 
