c5e6ee0d9f57c52d649ead662dcca617e324127d
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2021-01-28 13:13:29 +0530

    clean

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,21 +1,11 @@
-const { response } = require('express');
 const express = require('express');
-const mongoose = require('mongoose');
-const fetch = require('node-fetch');
-const helper = require('./helper');
-
 const respSchema=require('../Model/responseSchema');
-
 const router=express.Router();
 router.use(express.json());
-
-
-
 
 /*curl -X PUT -H "Content-Type: application/json" -d '{"data":[{ "is_choice":true,"skills":{"c":1,"python":1,"ruby":0} }]}' http://localhost:5003/api/response/5f80c8321d4dcf2434a9a0c5
 */
 router.put("/response/:responseId", async (req, res) => {
-       
   const responseId = req.params.responseId; 
   const response = await respSchema.update({"_id": responseId},{$push:{"sections":req.body.data}});
   res.json({"status":"Added"});
