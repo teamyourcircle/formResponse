de2bc51c5d4c65d14b06bd7e68ad0f912718b501
hardikshah197 <hardikshah.hs2015@gmail.com>
2021-03-19 19:41:33 +0530

    auto-present pending

--- MiddleWareFun/formDataExtracter.js
+++ MiddleWareFun/formDataExtracter.js
@@ -11,37 +11,43 @@ const dataExtracter = (req, res, next) => {
     else{
         req.form = formObj[0];
     }
-    const url = `http://localhost:5002/form/api/get/form/${req.form.form_id}`;
-    fetch(url).then( resp => resp.json())
-    .then(data => {
-        const responses = data.responseArray;
-        for(var i=0;i<responses.length;i++){
-            let sections = responses[i]['sections'];
-            sections.map(s => {
-                s.map(f =>{
-                    var key = Object.keys(f)[1];
-                    if(f["is_choice"]===false){
-                        if(responseSummary.hasOwnProperty(key)){
-                            responseSummary[key] = [...responseSummary[key],f[key]];
+    const token = req.headers['access-token'];
+    const reply = check_already_presentedSheet(token);
+
+    if(!reply) {
+        const url = `http://localhost:5002/form/api/get/form/${req.form.form_id}`;
+        fetch(url).then( resp => resp.json())
+        .then(data => {
+            const responses = data.responseArray;
+            for(var i=0;i<responses.length;i++){
+                let sections = responses[i]['sections'];
+                console.log(sections);
+                sections.map(s => {
+                    s.map(f =>{ 
+                        var key = Object.keys(f)[1];
+                        if(f["is_choice"]===false){
+                            if(responseSummary.hasOwnProperty(key)){
+                                responseSummary[key] = [...responseSummary[key],f[key]];
+                            }
+                            else{
+                                responseSummary[key]  =  [ f[key] ];
+                            }
+                        }else{
+                            if(choiceresponseSummary.hasOwnProperty(key)){
+                                choiceresponseSummary[key] = [...choiceresponseSummary[key],f[key]];
+                            }
+                            else{
+                                choiceresponseSummary[key]  =  [ f[key] ];
+                            }
                         }
-                        else{
-                            responseSummary[key]  =  [ f[key] ];
-                        }
-                    }else{
-                        if(choiceresponseSummary.hasOwnProperty(key)){
-                            choiceresponseSummary[key] = [...choiceresponseSummary[key],f[key]];
-                        }
-                        else{
-                            choiceresponseSummary[key]  =  [ f[key] ];
-                        }
-                    }
+                    })
                 })
-            })
-        }
-        req.my_formData = {...responseSummary,...choiceresponseSummary};
-        next();
-    })
-    .catch(err => res.status(502).json({"msg": "Bad Gateway - " + err.message}))
+            }
+            req.my_formData = {...responseSummary,...choiceresponseSummary};
+            next();
+        })
+        .catch(err => res.status(502).json({"msg": "Bad Gateway - " + err.message}))
+    }
 }
 
 module.exports = dataExtracter;
@@ -51,4 +57,35 @@ const validate_form_created_by_current_user  = (formsByUser, form_id) =>{
         return ;
     }
     return formsByUser.filter(form => form.form_id==form_id);
+}
+
+const check_already_presentedSheet = (token) => {
+    var flag = false;
+    const uri = "http://localhost:5000/auth/api/user/oauthApps";
+    const options = {
+        method: 'GET',
+        headers: {
+            'access-token': token,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }
+    fetch(uri, options)
+    .then(res => res.json())
+    .then( data => {
+        if(data.msg){
+            return res.status(403).json({'msg':`err :: ${data.msg}`});
+        }
+        req.socialID = data["integartionList"][0]["social_id"];
+        var add_info = data["integartionList"][0]['additional_info'];
+        Object.keys(add_info).forEach( e => {
+            if(e == req.form.form_id) {
+                var sheetUrl = `https://docs.google.com/spreadsheets/d/${add_info[e].spreadsheet_id}/edit#gid=${add_info[e].sheetId}`;
+                flag=true;
+                return res.status(200).json({'Sheet_URL': sheetUrl});
+            }
+        })
+        return flag;
+    })
+    .catch(err => res.status(502).json({"msg": "Bad Gateway - " + err.message}));
 }
