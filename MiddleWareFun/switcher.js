bc393fa3f680b64c3554c94b859b5a4e2721ab0b
hardikshah197 <hardikshah.hs2015@gmail.com>
2021-04-01 19:35:39 +0530

    Done with Edge cases

--- MiddleWareFun/switcher.js
+++ MiddleWareFun/switcher.js
@@ -18,56 +18,64 @@ const switcher_to = (req, res, next) => {
         if(data["integartionList"] == undefined)
             return res.status(404).json({'msg': 'Data not found'});
         data["integartionList"].forEach(ele => {
-            if(ele.email == undefined)
-                return res.status(404).json({'msg': 'supportive email is not found'});
+            // if(ele == undefined)
+            //     return res.status(404).json({'msg': 'supportive email is not found'});
             if(ele.email == req.body.switch_to)
                 req.switch_to = ele;
             if(ele.email == req.body.switch_from)
                 req.switch_from = ele;
         });
-        const integ_id = req.switch_to.integration_id;
-        const sheetId = req.switch_to.additional_info[formId].sheetId;
-        const spreadId = req.switch_to["additional_info"][formId].spreadsheet_id;
-        const uri = `http://localhost:5000/auth/api/oauth/edit/${integ_id}`;
-        const option = {
-            method: 'PUT',
-            headers: {
-                'access-token': token,
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'supportive_email': req.body.switch_to,
-            },
-            body:JSON.stringify({   
-                [formId] : {
-                    "spreadsheet_id": spreadId,
-                    "sheetId": sheetId,
-                    "deleted": false
-                }
-            })
-        }
-        fetch(uri, option)
-        .then(resp => resp.json())
-        .then( data => {
-            const uri_from = `http://localhost:5000/auth/api/oauth/edit/remove/${integ_id}`;
-            const option_from = {
+        if(req.switch_to != undefined ) {
+            const integ_id = req.switch_to.integration_id;
+            const sheetId = req.switch_to.additional_info[formId].sheetId;
+            const spreadId = req.switch_to["additional_info"][formId].spreadsheet_id;
+            const uri = `http://localhost:5000/auth/api/oauth/edit/${integ_id}`;
+            const option = {
                 method: 'PUT',
                 headers: {
                     'access-token': token,
                     'Content-Type': 'application/json',
                     'Accept': 'application/json',
-                    'supportive_email': req.body.switch_from,
+                    'supportive_email': req.body.switch_to,
                 },
-                body:JSON.stringify({
-                    "property" : formId
+                body:JSON.stringify({   
+                    [formId] : {
+                        "spreadsheet_id": spreadId,
+                        "sheetId": sheetId,
+                        "deleted": false
+                    }
                 })
             }
-            fetch(uri_from, option_from)
+            fetch(uri, option)
+            .then(resp => resp.json())
             .then( data => {
-                next();
+                if(req.switch_from != undefined) {
+                    const uri_from = `http://localhost:5000/auth/api/oauth/edit/remove/${integ_id}`;
+                    const option_from = {
+                        method: 'PUT',
+                        headers: {
+                            'access-token': token,
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json',
+                            'supportive_email': req.body.switch_from,
+                        },
+                        body:JSON.stringify({
+                            "property" : formId
+                        })
+                    }
+                    fetch(uri_from, option_from)
+                    .then( data => {
+                        next();
+                    })
+                    .catch( err => res.status(400).json({'msg' : `error : : ${err}`}))
+                }
+                else
+                    return res.status(404).json({'msg': 'supportive email_from is not found'});
             })
             .catch( err => res.status(400).json({'msg' : `error : : ${err}`}))
-        })
-        .catch( err => res.status(400).json({'msg' : `error : : ${err}`}))
+        }
+        else 
+        return res.status(404).json({'msg': 'supportive email_to is not found'});
     })
     .catch(err => res.status(400).json({'msg' : `error : : ${err}`}));
 }
--- Routes/consumer.js
+++ Routes/consumer.js
@@ -106,7 +106,7 @@ const get_info_for_consumer = async (queueName,token,formId) =>{
             for(let i=0;i<integration_list.length;i++){
                 let {email,additional_info} = integration_list[i];
                 emails.push(email);
-                if(additional_info && additional_info[formId]){
+                if(additional_info && additional_info[formId] && !additional_info[formId].deleted){
                     active_email = email;
                 }
             }
