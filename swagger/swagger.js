396b60811f70fbad77361278a08570bd7b813501
hardikshah197 <hardikshah.hs2015@gmail.com>
2021-08-03 12:06:54 +0530

    swagger base url updated

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -4,7 +4,9 @@ const path = require('path');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load(path.join(__dirname, '/swagger.yaml'));
 const router = require('express').Router();
-
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+swaggerDocument.host=config.FORM_RESPONSE_BASE_URL;
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
